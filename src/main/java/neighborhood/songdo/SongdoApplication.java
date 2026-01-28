09b38880b9d0bb80dc99803f2f0b5945e2f3fe03
Huiyeongkim <huiyeong9619@naver.com>
2026-01-28 17:58:25 +0900

    init

---
+++ src/main/java/neighborhood/songdo/SongdoApplication.java
@@ -0,0 +1,13 @@
+package neighborhood.songdo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongdoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongdoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/neighborhood/songdo/SongdoApplicationTests.java
@@ -0,0 +1,13 @@
+package neighborhood.songdo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongdoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
