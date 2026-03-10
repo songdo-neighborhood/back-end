c229942e9123293c4b625498456cfcced8a5350a
Huiyeongkim <huiyeong9619@naver.com>
2026-03-10 19:55:13 +0900

    feat: payment repository 저장

---
+++ src/main/java/neighborhood/songdo/payment/repository/PaymentRepository.java
@@ -0,0 +1,11 @@
+package neighborhood.songdo.payment.repository;
+
+import neighborhood.songdo.payment.domain.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+    Optional<Payment> findByOrderId(Long orderId);
+}
